48f1b8e737a2e1584f1420919c60a13b405d4472
Simon Szuharev <simonszuharev@gmail.com>
2021-04-15 22:14:50 -0500

    Create data.py

---
+++ day 17 (Quizz)/data.py
@@ -0,0 +1,16 @@
+question_data = [
+    {"text": "A slug's blood is green.", "answer": "true"},
+    {"text": "The loudest animal is the African Elephant.", "answer": "false"},
+    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "true"},
+    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "true"},
+    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to "
+             "eat.", "answer": "true"},
+    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.",
+     "answer": "false"},
+    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "true"},
+    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "false"},
+    {"text": "Google was originally called 'Backrub'.", "answer": "true"},
+    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "true"},
+    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "false"},
+    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "true"}
+]
