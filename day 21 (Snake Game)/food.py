aee515fe886fa9141458469c08762f66a21b1caa
Simon Szuharev <simonszuharev@gmail.com>
2021-04-20 19:25:47 -0500

    Create food.py

---
+++ day 21 (Snake Game)/food.py
@@ -0,0 +1,19 @@
+import random
+from turtle import Turtle
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.7, stretch_wid=0.7)
+        self.color("tomato")
+        self.speed("fastest")
+        self.refresh_location()
+
+    def refresh_location(self):
+        random_x = random.randint(-270, 270)
+        random_y = random.randint(-270, 270)
+        self.goto(random_x, random_y)
