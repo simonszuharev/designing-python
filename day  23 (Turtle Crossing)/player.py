14817b86f580529928cdbded3a0fac11136d1f98
Simon Szuharev <simonszuharev@gmail.com>
2021-04-26 16:35:19 -0500

    Update player.py

--- day  23 (Turtle Crossing)/player.py
+++ day  23 (Turtle Crossing)/player.py
@@ -2,7 +2,7 @@ import turtle
 from turtle import Turtle
 
 STARTING_POSITION = (0, -280)
-MOVE_DISTANCE = 20
+MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
@@ -16,11 +16,17 @@ class Player(Turtle):
         self.shape("turtle")
         self.color("black")
         self.penup()
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
         self.setheading(90)
 
     def __move(self):
         self.forward(MOVE_DISTANCE)
+
+    def __go_to_start_now(self):
+        self.goto(STARTING_POSITION)
+
+    def go_to_start(self):
+        self.__go_to_start_now()
 
     def create(self):
         self.__create_the_turtle()
@@ -28,5 +34,8 @@ class Player(Turtle):
     def move(self):
         self.__move()
 
-    def start_over(self):
-        turtle.goto(STARTING_POSITION)
+    def finished(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
