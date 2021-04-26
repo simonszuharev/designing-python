84fc5e54dfcdcb5ac4079238b025ee7f237d2ae4
Simon Szuharev <simonszuharev@gmail.com>
2021-04-26 16:34:59 -0500

    Update car_manager.py

--- day  23 (Turtle Crossing)/car_manager.py
+++ day  23 (Turtle Crossing)/car_manager.py
@@ -2,43 +2,46 @@ import random
 from turtle import Turtle
 
 COLORS = ["red", "orange", "yellow", "pink", "blue", "purple"]
-STARTING_MOVE_DISTANCE = 2
-MOVE_INCREMENT = 5
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
 
 
 class CarManager:
 
     def __init__(self):
-        self.car = []
-        self.create()
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
 
     def __create_the_car(self):
-        y_cor = random.randint(-280, 280)
-        x_cor = random.randint(280, 500)
-        color = random.choice(COLORS)
-        for i in range(0, 2):
-            i = Turtle(shape="square")
-            i.penup()
-            i.color(color)
-            i.goto(x=x_cor, y=y_cor)
-            i.setheading(180)
-            self.car.append(i)
-            x_cor -= 10
-        return self.car
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            color = random.choice(COLORS)
+            new_car = Turtle(shape="square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(color)
+            y_cor = random.randint(-250, 250)
+            new_car.goto(300, y_cor)
+            new_car.setheading(180)
+            self.all_cars.append(new_car)
 
-    def __move(self, speed):
-        speed = STARTING_MOVE_DISTANCE + (MOVE_INCREMENT * speed)
-        y_cor = self.car[0].ycor()
-        for item_num in range(len(self.car)):
-            new_x = self.car[item_num].xcor() - speed
-            self.car[item_num-1].goto(new_x, y_cor)
-        self.car[item_num].forward(speed+20)
+    def __move(self):
+        for car in self.all_cars:
+            car.forward(self.car_speed)
 
-    def move(self, speed):
-        self.__move(speed)
+    def __level_up(self):
+        self.car_speed += MOVE_INCREMENT
+
+    def level_up(self):
+        self.__level_up()
+
+    def move(self):
+        self.__move()
 
     def create(self):
         self.__create_the_car()
+
+
 
 
 
