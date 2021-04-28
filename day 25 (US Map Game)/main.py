c7bb5795fd15808edcc15492e0824e917fb20fc8
Simon Szuharev <simonszuharev@gmail.com>
2021-04-28 18:52:37 -0500

    Update main.py

--- day 25 (US Map Game)/main.py
+++ day 25 (US Map Game)/main.py
@@ -14,16 +14,23 @@ data = pandas.read_csv("50_states.csv")
 list_of_states = data["state"].to_list()
 list_of_x_cor = data["x"].to_list()
 list_of_y_cor = data["y"].to_list()
+states_to_learn = list_of_states
 
 while correct_guesses < 50:
-    answer = screen.textinput(title=f"{correct_guesses}/50 State Name", prompt="Guess the state?")
+    answer = screen.textinput(title=f"{correct_guesses}/50 State Name", prompt="Guess the state? to exit type 'Exit'")
     if answer.capitalize() in list_of_states:
         index = list_of_states.index(answer.capitalize())
         state = StateOnMap(list_of_x_cor[index], list_of_y_cor[index], list_of_states[index])
         correct_guesses += 1
+        states_to_learn.remove(answer.capitalize())
+
+    elif answer.capitalize() == "Exit":
+        new_data = pandas.DataFrame(states_to_learn)
+        new_data.to_csv("states_to_learn.csv")
+        break
     else:
         print("Try again.")
-        
+
 
 screen.exitonclick()
 
